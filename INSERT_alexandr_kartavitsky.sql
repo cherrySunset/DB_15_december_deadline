3f78a3da933cbbb3a453dc44dfdf414a267684d6
cherrySunset <128701786+cherrySunset@users.noreply.github.com>
2023-12-14 20:55:52 +0300

    Add files via upload

---
+++ INSERT_alexandr_kartavitsky.sql
@@ -0,0 +1,18 @@
+insert into film
+values ('1001', 'Lords of Chaos', 'biographical thriller directed by Jonas Åkerlund, dedicated to the tragic events surrounding the Norwegian black metal scene of the early 1990s',
+	   '2018', '1', '1', '14', '4.99', '112', '15.99', 'R', current_date, '{Trailers}',  'guitar:19 confront:14 documentari:11 evil:16 mile:2 must:13 spi:1 thrill:17');
+
+insert into actor
+values ('201', 'Rory', 'Culkin', current_date),
+('202', 'Emory', 'Cohen', current_date),
+('203', 'Jack', 'Kilmer', current_date);
+
+
+insert into actor
+values ('201', 'Rory', 'Culkin', current_date),
+('202', 'Emory', 'Cohen', current_date),
+('203', 'Jack', 'Kilmer', current_date);
+
+
+insert into inventory
+values ('4582', '1001', '1', current_date);
